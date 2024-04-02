a031a786fd6a65fc28ee415d4892b61b1ed14f7b
Jack-WebDev <Jackbuda31@gmail.com>
2024-04-02 12:27:46 +0200

    fix dist bug. Add env variables

--- backend/db/knexfile.js
+++ backend/db/knexfile.js
@@ -7,9 +7,9 @@ module.exports = {
   development: {
     client: "pg",
     connection: {
-      database: "nsfas-form",
-      user: "postgres",
-      password: "postgres",
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
     },
     pool: {
       min: 2,
